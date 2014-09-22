e41482366f72947b08b0920ca87dff03a46aa855
kokobeens <collinkokotas@gmail.com>
2014-09-22 15:13:47 -0700

    Wrap THREEjs in an angular factory

---
+++ client/THREEjsAngular.js
@@ -0,0 +1,29 @@
+angular.module('THREE', [])
+  .factory('threeService', ['$document', '$q', '$rootScope', function($document, $q, $rootScope){
+    var d = $q.defer();
+    var onScriptLoad = function(){
+      $rootScope.$apply(function(){
+        d.resolve(window.THREE);
+      });
+    };
+
+    var scriptTag = $document[0].createElement('script');
+    scriptTag.type = 'text/javascript';
+    scriptTag.async = true;
+    scriptTag.src = '../node_modules/n3d-threejs/index.js';
+    scriptTag.onreadystatechange = function(){
+      if(this.readyState === 'complete'){
+        onScriptLoad();
+      }
+    };
+    scriptTag.onload = onScriptLoad;
+
+    $document[0].getElementsByTagName('body')[0]
+      .appendChild(scriptTag);
+
+    return {
+      THREE: function(){
+        return d.promise;
+      }
+    }
+  }]);
