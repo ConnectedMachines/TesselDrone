fec581de85232be4953971145b7675dc492e6e3e
Geoffrey Abdallah <gabdallah222@gmail.com>
2014-09-22 19:34:52 -0700

    Fixed server issue

--- client/drone_control/controller.js
+++ client/drone_control/controller.js
@@ -9,23 +9,23 @@ angular.module('MadProps')
       $scope.drone.status = "Checking motors";
       Requests.preflight()
         .then(function(status){
-          $scope.drone.status = status;
+          $scope.drone.status = "Ready to fly";
         })
     }
 
     $scope.takeOff = function(){
       $scope.drone.status = "Taking Off";
       Requests.takeOff()
-        .then(function(status){
-          $scope.drone.status = status;
+        .then(function(){
+          $scope.drone.status = "Successful Takeoff";
         });
     };
     
     $scope.land = function(){
       $scope.drone.status = "Landing";
       Requests.land()
-        .then(function(status){
-          $scope.drone.status = status;
+        .then(function(){
+          $scope.drone.status = "Successfully Landed";
         });
     };
   })
@@ -35,28 +35,27 @@ angular.module('MadProps')
     var preflight = function(){
       return $http({
         method: 'GET',
-        url: '10.8.31.216:8000/preflight'
-      }).success(function(){
-        return status = "Ready to fly"
+        url: '//10.8.31.216:8000/preflight'
+      }).success(function(data){
+        return "Ready to fly"
       })
     };
     var takeOff = function(){
       return $http({
         method: 'GET',
-        url: '10.8.31.216:8000/takeOff'
+        url: '//10.8.31.216:8000/takeOff'
       })
-      .success(function(){
-        status = "Successful Takeoff";
-        console.log(status)
+      .success(function(data){
+        return "Successful Takeoff";
       });
     };
 
     var land = function(){
       return $http({
         method: 'GET',
-        url: '10.8.31.216:8000/land'
+        url: '//10.8.31.216:8000/land'
       }).success(function(){
-        return status = "Successfully Landed";
+        return "Successfully Landed";
       });
     };
 
