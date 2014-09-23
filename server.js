50613a4c9caf8df188cb5720ce5402c0a2836bc9
Geoffrey Abdallah <gabdallah222@gmail.com>
2014-09-22 17:20:44 -0700

    Added websocket

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var ws = require("nodejs-websocket");
 var app = express();
 
 var port = process.env.PORT || 3000;
+var tesselport = 8000;
 
 app.use(express.static(__dirname));
 
@@ -12,3 +14,16 @@ app.get('/', function(req, res){
 console.log("listening on port", port);
 console.log(__dirname)
 app.listen(port);
+
+
+// INSERT TESSEL IP ADDRESS HERE. Always prepend with 'ws://' to indicate websocket
+var connection = ws.connect('ws://10.8.31.216:' + tesselport, function() {
+  // When we connect to the server, send some catchy text
+  connection.sendText("My milkshake brings all the boys to the yard")
+});
+
+// When we get text back
+connection.on('text', function(text) {
+  // print it out
+  console.log("Echoed back from server:", text);
+})
