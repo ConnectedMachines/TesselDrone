c76c05a8d4a5e0a362653b7c9a728d43b6c7dff5
kokobeens <collinkokotas@gmail.com>
2014-09-29 11:29:10 -0700

    Setup path to recieve tessel data

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors')
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 3000;
@@ -7,11 +8,16 @@ var port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
   res.redirect('/client');
 })
 
+app.post('/test', function(req, res){
+  console.log('REQUEST:', req.body);
+  res.send('recieved!');
+})
+
 console.log("listening on port", port);
-console.log(__dirname)
 app.listen(port);
