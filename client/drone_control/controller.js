661cd84f6fb4f5ce0ca4dd7c74fd02a016f2f3e4
Geoffrey Abdallah <gabdallah222@gmail.com>
2014-10-06 21:57:31 -0700

    Added in tests for the DroneCommandController

--- client/drone_control/controller.js
+++ client/drone_control/controller.js
@@ -1,23 +1,23 @@
 angular.module('MadProps')
   .controller('DroneCommandController', function ($scope, Requests, socket) {
     $scope.drone = {};
-    $scope.drone.status = "idle";
+    $scope.drone.status = "Idle";
+    $scope.emitCondition = Requests.emitCondition;
     //Possible Socket.io
     //Angular socket io
 
-
     $scope.preflight = function () {
-      $scope.drone.status = "Checking motors";
-      Requests.emitCondition('preflight');
+      $scope.drone.status = "Checking Motors";
+      $scope.emitCondition('preflight');
     };
 
     $scope.takeOff = function () {
       $scope.drone.status = "Taking Off";
-      Requests.emitCondition('takeoff');
+      $scope.emitCondition('takeoff');
     };
     $scope.land = function () {
       $scope.drone.status = "Landing";
-      Requests.emitCondition('land');
+      $scope.emitCondition('land');
     };
   })
 
