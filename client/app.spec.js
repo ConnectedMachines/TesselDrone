11b85660754abbb8269ff9a6bb13827efac77830
Mike Luby <Michael@mLuby.com>
2014-10-07 21:56:33 -0700

    100% coverage of MasterController.js without abusing $scope

--- client/app.spec.js
+++ client/app.spec.js
@@ -60,34 +60,19 @@ describe('Client', function() {
         $scope: scope
       });
     }));
-    it('on d3Loaded, that flag becomes true', function () {
-      expect(scope.d3VisualizerLoaded).toBe(false);
-      scope.$broadcast('d3Loaded');
-      expect(scope.d3VisualizerLoaded).toBe(true);
-    });
-    it('on threeLoaded, that flag becomes true', function () {
-      expect(scope.threeVisualizerLoaded).toBe(false);
-      scope.$broadcast('threeLoaded');
-      expect(scope.threeVisualizerLoaded).toBe(true);
-    });
     it('on socket:droneData, only if both loaded flags are true, then broadcasts.', function () {
-      scope.$broadcast('socket:droneData');
-      expect(scope.data).not.toBeDefined();
-      scope.d3VisualizerLoaded = true;
-      scope.threeVisualizerLoaded = true;
-      var data = JSON.stringify('{attitude:0, motorThrottle:0}');
-      scope.$broadcast('socket:droneData', data);
-      expect(scope.data).toBeDefined();
+      spyOn(scope, '$broadcast');
+      var broadcastingScope = scope.$parent;
+      broadcastingScope.$broadcast('socket:droneData', data);
+      expect(scope.$broadcast).not.toHaveBeenCalledWith('throttleData', undefined);
+      expect(scope.$broadcast).not.toHaveBeenCalledWith('attitudeData', undefined);
+      broadcastingScope.$broadcast('d3Loaded');
+      broadcastingScope.$broadcast('threeLoaded');
+      var data = JSON.stringify('{attitude: 0, motorThrottles: 0}');
+      broadcastingScope.$broadcast('socket:droneData', data);
+      expect(scope.$broadcast).toHaveBeenCalledWith('throttleData', undefined);
+      expect(scope.$broadcast).toHaveBeenCalledWith('attitudeData', undefined);
     });
-
-    // it("should broadcast something", function() {
-    //   spyOn(scope, '$broadcast');
-    //   scope.$broadcast('d3Loaded');
-    //   scope.$broadcast('threeLoaded');
-    //   scope.$broadcast('socket:droneData');
-    //   expect(scope.$broadcast).toHaveBeenCalledWith('attitudeData');
-    //   expect(scope.$broadcast).toHaveBeenCalledWith('throttleData');
-    // });
 
     it('has listeners on \'d3Loaded\', \'threeLoaded\', and \'socket:droneData\'', function(){
       expect(scope.$$listeners.d3Loaded).toBeDefined();
@@ -95,39 +80,4 @@ describe('Client', function() {
       expect(scope.$$listeners['socket:droneData']).toBeDefined();
     });
   });
-    // it('listens for \'d3Loaded\'', function(){
-      // $rootScope.$broadcast("d3Loaded");
-      // console.log(scope.$on.mostRecentCall.args);      
-      // socket.forward('d3Loaded', $scope);
-      // console.log(scope.$$listeners.d3Loaded).toBeDefined();
-    // });
-    // it('on \'d3Loaded\', sets to true', function() {
-    //   console.log(scope.$$listeners.contains);
-    //   console.log(scope.$on);
-    //   // scope.$emit('d3Loaded');
-    //   expect(scope.$on).toHaveBeenCalled();
-    // });
-    // it('initializes loaders to false;', function() {
-    //   console.log(this);
-    //   expect(d3VisualizerLoaded).toBe(false);
-    //   expect(threeVisualizerLoaded).toBe(false);
-    // });
-    // it('on \'threeLoaded\', sets to true', function() {
-    //   scope.$emit('threeLoaded');
-    //   expect(threeVisualizerLoaded).toBe(true);
-    // });
-    // it('on \'socket:droneData\', broadcast drone data', function() {
-    //   d3VisualizerLoaded = true;
-    //   threeVisualizerLoaded = true;
-    //   scope.$emit('socket:droneData', 'alice', {attitude:'bob', motorThrottles:'carl'});
-
-    //   expect(data.attitude).toBeDefined();
-    //   expect(data.attitude).toBe('bob');
-    //   expect(data.motorThrottles).toBeDefined();
-    //   expect(data.motorThrottles).toBe('carl');
-    //   // expect broadcast 'attitudeData' to contain data.attitude.
-    //   // expect broadcast 'throttleData' to contain data.motorThrottles.
-    //   });
-    // });
-
 });
--- client/masterController.js
+++ client/masterController.js
@@ -1,19 +1,19 @@
 angular.module('MadProps')
   .controller('MasterController', ['$scope', '$http', 'socket', function($scope, $http, socket){
-    $scope.d3VisualizerLoaded = false;
-    $scope.threeVisualizerLoaded = false;
+    var d3VisualizerLoaded = false;
+    var threeVisualizerLoaded = false;
 
     $scope.$on('d3Loaded', function(){
-      $scope.d3VisualizerLoaded = true;
+      d3VisualizerLoaded = true;
     });
     $scope.$on('threeLoaded', function(){
-      $scope.threeVisualizerLoaded = true;
+      threeVisualizerLoaded = true;
     });
     $scope.$on('socket:droneData', function (ev, data) {
-      if($scope.d3VisualizerLoaded && $scope.threeVisualizerLoaded){
-        $scope.data = JSON.parse(data);
-        $scope.$broadcast('attitudeData', $scope.data.attitude);
-        $scope.$broadcast('throttleData', $scope.data.motorThrottles);
+      if(d3VisualizerLoaded && threeVisualizerLoaded){
+        data = JSON.parse(data);
+        $scope.$broadcast('attitudeData', data.attitude);
+        $scope.$broadcast('throttleData', data.motorThrottles);
       }
     });
   }]);
