1f6285250d0b318036dba598cf3ff4377eff51a1
Geoffrey Abdallah <gabdallah222@gmail.com>
2014-10-09 10:37:49 -0700

    Most recent iteration of PID flight tests

--- tessel/stabilize.js
+++ tessel/stabilize.js
@@ -1,4 +1,5 @@
 var mainControl = require('./mainControl.js');
+var servo = mainControl.servo;
 var throttleIncrement = mainControl.throttleIncrement;
 var motorMaxThrottle = mainControl.motorMaxThrottle;
 var PIDoutput = require('./PID.js').PIDoutput;
@@ -23,11 +24,24 @@ var whichAxisToStabilize = function(axis){
       mainControl.axisChanging[axis] = true;
       stabilize(posMotor, negMotor, axis);
     } 
-    else if (!mainControl.axisChanging[axis]){
-      throttleUp(1, 'x');
-      throttleUp(2, 'x');
-      throttleUp(3, 'y');
-      throttleUp(4, 'y');
+    else if (!mainControl.axisChanging['x'] && !mainControl.axisChanging['y']){
+      console.log("THROTTLE IT UP")
+      servo.move(1, mainControl.motors[1].currentThrottle + throttleIncrement, function(){
+        mainControl.motors[1].currentThrottle = mainControl.motors[1].currentThrottle + throttleIncrement;
+        mainControl.lobot.speak();        
+      })
+      servo.move(2, mainControl.motors[2].currentThrottle + throttleIncrement, function(){
+        mainControl.motors[2].currentThrottle = mainControl.motors[2].currentThrottle + throttleIncrement;
+        mainControl.lobot.speak();
+      })
+      servo.move(3, mainControl.motors[3].currentThrottle + throttleIncrement, function(){
+        mainControl.motors[3].currentThrottle = mainControl.motors[3].currentThrottle + throttleIncrement;
+        mainControl.lobot.speak(); 
+      })
+      servo.move(4, mainControl.motors[4].currentThrottle + throttleIncrement, function(){
+        mainControl.motors[4].currentThrottle = mainControl.motors[4].currentThrottle + throttleIncrement;
+        mainControl.lobot.speak();
+      });
     }
     setImmediate(function(){
       whichAxisToStabilize(axis); //After balancing mainControl.motors call balanceAxis
