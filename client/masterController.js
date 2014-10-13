c5743532cd7ded7dbd01bca5aa750a6ee26d3943
kokobeens <collinkokotas@gmail.com>
2014-10-13 14:34:55 -0700

    Added new propeller models

--- client/masterController.js
+++ client/masterController.js
@@ -3,6 +3,24 @@ angular.module('MadProps')
     var d3VisualizerLoaded = false;
     var threeVisualizerLoaded = false;
 
+    _override = {};
+    Object.defineProperty(_override, 'attitude', {
+      get: function(){
+        console.error('ERROR: Cannot retrieve');
+      },
+      set: function(arr){   // Math.round(arr[0]*(Math.PI/180))
+        $scope.$broadcast('attitudeData', {pitch: arr[0]*(Math.PI/180), yaw: arr[1]*(Math.PI/180), roll: arr[2]*(Math.PI/180)});
+      }
+    });
+    Object.defineProperty(_override, 'throttle', {
+      get: function(){
+        console.error('ERROR: Cannot retrieve');
+      },
+      set: function(arr){
+        $scope.$broadcast('throttleData', {motor1: arr[0], motor2: arr[1], motor3: arr[2], motor4: arr[3]});
+      }
+    });
+
     $scope.$on('d3Loaded', function(){
       d3VisualizerLoaded = true;
     });
