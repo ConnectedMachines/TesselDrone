e11e444136922c642b6e4f60d0d041dfbb74f9cf
kokobeens <collinkokotas@gmail.com>
2014-10-13 21:26:18 -0700

    Add more override functions

--- client/masterController.js
+++ client/masterController.js
@@ -5,19 +5,30 @@ angular.module('MadProps')
 
     _override = {};
     Object.defineProperty(_override, 'attitude', {
-      get: function(){
-        console.error('ERROR: Cannot retrieve');
-      },
       set: function(arr){   // Math.round(arr[0]*(Math.PI/180))
         $scope.$broadcast('attitudeData', {pitch: arr[0]*(Math.PI/180), yaw: arr[1]*(Math.PI/180), roll: arr[2]*(Math.PI/180)});
       }
     });
     Object.defineProperty(_override, 'throttle', {
-      get: function(){
-        console.error('ERROR: Cannot retrieve');
-      },
       set: function(arr){
         $scope.$broadcast('throttleData', {motor1: arr[0], motor2: arr[1], motor3: arr[2], motor4: arr[3]});
+      }
+    });
+    Object.defineProperty(_override, 'tumble', {
+      set: function(bool){
+        $scope._tumble = bool;
+      }
+    });
+    Object.defineProperty(_override, 'present', {
+      set: function(bool){
+        if(bool){
+          this.tumble = true;
+          this.throttle = [0.8, 0.8, 0.8, 0.8];
+        }else{
+          this.tumble = false;
+          this.throttle = [0,0,0,0];
+          this.attitude = [0,0,0];
+        }
       }
     });
 
